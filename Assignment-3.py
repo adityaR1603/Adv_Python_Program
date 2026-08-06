9eede62203de976ba60ef359127bd072d59283cd
Aditya Raj <adityaraj16217@gmail.com>
2026-08-06 10:08:42 +0530

    Update Assignment-3.py

--- Assignment-3.py
+++ Assignment-3.py
@@ -1,29 +1,55 @@
-# Strategy Classes
-
-class CreditCard:
-
+class PaymentStrategy:
     def pay(self, amount):
-        print("Paid ₹", amount, "using Credit Card")
+        pass
 
-
-class UPI:
-
+class CreditCardPayment(PaymentStrategy):
     def pay(self, amount):
-        print("Paid ₹", amount, "using UPI")
+        print(f"₹{amount} paid using Credit Card.")
 
-class Payment:
+class DebitCardPayment(PaymentStrategy):
+    def pay(self, amount):
+        print(f"₹{amount} paid using Debit Card.")
 
+class UPIPayment(PaymentStrategy):
+    def pay(self, amount):
+        print(f"₹{amount} paid using UPI.")
+
+class NetBankingPayment(PaymentStrategy):
+    def pay(self, amount):
+        print(f"₹{amount} paid using Net Banking.")
+
+class PaymentProcessor:
     def __init__(self, strategy):
         self.strategy = strategy
 
-    def make_payment(self, amount):
+    def set_strategy(self, strategy):
+        self.strategy = strategy
+
+    def process_payment(self, amount):
         self.strategy.pay(amount)
 
+# Main Program
+amount = float(input("Enter Payment Amount: ₹"))
 
-# Using Credit Card
-p = Payment(CreditCard())
-p.make_payment(1000)
+print("\nSelect Payment Method")
+print("1. Credit Card")
+print("2. Debit Card")
+print("3. UPI")
+print("4. Net Banking")
 
-# Using UPI
-p = Payment(UPI())
-p.make_payment(500)
+choice = int(input("Enter your choice: "))
+
+if choice == 1:
+    payment = CreditCardPayment()
+elif choice == 2:
+    payment = DebitCardPayment()
+elif choice == 3:
+    payment = UPIPayment()
+elif choice == 4:
+    payment = NetBankingPayment()
+else:
+    print("Invalid choice!")
+    exit()
+
+processor = PaymentProcessor(payment)
+processor.process_payment(amount)
