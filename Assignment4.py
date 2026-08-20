1efc035b2a07cea8b27b712a2c1bbd43cd8dbcc5
Aditya Raj <adityaraj16217@gmail.com>
2026-08-20 20:57:59 +0530

    Refactor Fibonacci functions and add output
    Restored and formatted the Fibonacci functions using memoization and tabulation. Added output for tabulation results.

--- Assignment4.py
+++ Assignment4.py
@@ -1,8 +1,8 @@
 # Memoization
 def fibonacci_memo(n, memo):
-
     if n <= 1:
         return n
+        
     if n in memo:
         return memo[n]
     memo[n] = fibonacci_memo(n - 1, memo) + fibonacci_memo(n - 2, memo)
